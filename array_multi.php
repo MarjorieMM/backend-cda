c864ab03135e9da5671a72945b63349b166c1475
Lucas Delobelle <lucas@ld-web.net>
2022-01-04 15:04:21 +0100

    :construction: Bootstrap code, users exercise

---
+++ array_multi.php
@@ -0,0 +1,43 @@
+<?php
+$people = [
+  [
+    'avatar' => 'images/avatar_1.jpg',
+    'name' => 'Barnes',
+    'firstname' => 'Marguerite',
+    'age' => '44'
+  ],
+  [
+    'avatar' => 'images/avatar_2.jpg',
+    'name' => 'Baker',
+    'firstname' => 'Gabriel',
+    'age' => '33'
+  ],
+  [
+    'avatar' => 'images/avatar_3.jpg',
+    'name' => 'Barker',
+    'firstname' => 'Ola',
+    'age' => '34'
+  ],
+  [
+    'avatar' => 'images/avatar_4.jpg',
+    'name' => 'Lopez',
+    'firstname' => 'Rosie',
+    'age' => '49'
+  ],
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Utilisateurs</title>
+</head>
+
+<body>
+  <!-- LISTE DES UTILISATEURS -->
+</body>
+
+</html>
