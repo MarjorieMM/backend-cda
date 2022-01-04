dab99eaaa333321cf1c71c6b2f15cd6cc33ce652
Lucas Delobelle <lucas@ld-web.net>
2022-01-04 16:52:29 +0100

    :sparkles: User exercise, foreach loop

--- array_multi.php
+++ array_multi.php
@@ -1,5 +1,5 @@
 <?php
-$people = [
+$peoples = [
   [
     'avatar' => 'images/avatar_1.jpg',
     'name' => 'Barnes',
@@ -24,6 +24,12 @@ $people = [
     'firstname' => 'Rosie',
     'age' => '49'
   ],
+  [
+    'avatar' => 'images/avatar_1.jpg',
+    'name' => 'Toto',
+    'firstname' => 'Tata',
+    'age' => '15'
+  ],
 ];
 ?>
 <!DOCTYPE html>
@@ -33,11 +39,28 @@ $people = [
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link href="array_multi.css" rel="stylesheet">
   <title>Utilisateurs</title>
 </head>
 
 <body>
-  <!-- LISTE DES UTILISATEURS -->
+  <h1>Utilisateurs</h1>
+  <div class="list">
+    <?php foreach ($peoples as $people) { ?>
+      <article class="item">
+        <div class="picture">
+          <img src="<?php echo $people['avatar']; ?>" alt="<?php echo $people['name'] . ' ' . $people['firstname']; ?>" />
+        </div>
+        <div class="infos">
+          <h3><?php echo $people['name'] . ' ' . $people['firstname']; ?></h3>
+          <h4><?php echo $people['age']; ?> ans</h4>
+        </div>
+      </article>
+    <?php } ?>
+  </div>
 </body>
 
 </html>
