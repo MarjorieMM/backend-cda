722375eb6cfb272063d7473b391a54470f9d6a07
Lucas Delobelle <lucas@ld-web.net>
2022-01-05 15:34:50 +0100

    :sparkles: functions practice

--- functions.php
+++ functions.php
@@ -2,9 +2,14 @@
 // Signature d'une fonction :
 // nom(parametres): type_retour
 
-function direBonjour(string $nom = "Bob"): void
+function direBonjour(string $nom = "Bob", bool $appendBrTag = true): void
 {
-  echo "Bonjour $nom !";
+  echo "Bonjour $nom !" . ($appendBrTag ? "<br />" : "");
+}
+
+function getParagraphUppercase(string $text): string
+{
+  return "<p>" . strtoupper($text) . "</p>";
 }
 
 function somme(int $a, int $b = 5): int
@@ -16,3 +21,7 @@ direBonjour();
 
 echo somme(2, 3);
 echo somme(8);
+
+$text = "blue plain zoo moment neighborhood contain pupil statement separate exact within trip than population steep wheel storm beginning seed ocean too rocky crowd donkey";
+
+echo getParagraphUppercase($text);
