38877569d702ef25b49ac8d87d55a92fd5614622
Lucas Delobelle <lucas@ld-web.net>
2022-01-05 16:17:35 +0100

    :sparkles: switch

--- functions.php
+++ functions.php
@@ -17,6 +17,33 @@ function somme(int $a, int $b = 5): int
   return $a + $b;
 }
 
+function formatAndUppercase(string $text, string $tag): string
+{
+  // if ($tag === "div" || $tag === "span" || $tag === "p") {
+  //   return "<" . $tag . ">" . strtoupper($text) . "</" . $tag . ">";
+  // } else {
+  //   return getParagraphUppercase($text);
+  // }
+
+  // Switch : structure conditionnelle
+  // On teste les valeurs possibles d'une variable au travers de cas
+  // Une instruction break sort du switch
+  switch ($tag) {
+    case 'div':
+    case 'span':
+      break;
+    case 'p':
+    default:
+      return getParagraphUppercase($text);
+  }
+
+  return "<" . $tag . ">" . strtoupper($text) . "</" . $tag . ">";
+}
+// Inverse de $tag === "div" || $tag === "span" || $tag === "p" :
+// On inverse les opérateurs de comparaison (=== devient !==)
+// et on inverse les opérateurs logiques (|| devient &&)
+// $tag !== "div" && $tag !== "span" && $tag !== "p"
+
 direBonjour();
 
 echo somme(2, 3);
@@ -25,3 +52,7 @@ echo somme(8);
 $text = "blue plain zoo moment neighborhood contain pupil statement separate exact within trip than population steep wheel storm beginning seed ocean too rocky crowd donkey";
 
 echo getParagraphUppercase($text);
+echo formatAndUppercase($text, "div");
+echo formatAndUppercase($text, "span");
+echo formatAndUppercase($text, "p");
+echo formatAndUppercase($text, "img");
