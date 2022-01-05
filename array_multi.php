524ce9ee86e2f3e917279518f2ddbfc2ddeaf053
Lucas Delobelle <lucas@ld-web.net>
2022-01-05 09:56:26 +0100

    :hammer: Refactoring peoples

--- array_multi.php
+++ array_multi.php
@@ -1,36 +1,6 @@
 <?php
-$peoples = [
-  [
-    'avatar' => 'images/avatar_1.jpg',
-    'name' => 'Barnes',
-    'firstname' => 'Marguerite',
-    'age' => '44'
-  ],
-  [
-    'avatar' => 'images/avatar_2.jpg',
-    'name' => 'Baker',
-    'firstname' => 'Gabriel',
-    'age' => '33'
-  ],
-  [
-    'avatar' => 'images/avatar_3.jpg',
-    'name' => 'Barker',
-    'firstname' => 'Ola',
-    'age' => '34'
-  ],
-  [
-    'avatar' => 'images/avatar_4.jpg',
-    'name' => 'Lopez',
-    'firstname' => 'Rosie',
-    'age' => '49'
-  ],
-  [
-    'avatar' => 'images/avatar_1.jpg',
-    'name' => 'Toto',
-    'firstname' => 'Tata',
-    'age' => '15'
-  ],
-];
+// Charger les données
+include 'peoples.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
---
+++ peoples.php
@@ -0,0 +1,34 @@
+<?php
+// définition des données
+$peoples = [
+  [
+    'avatar' => 'images/avatar_1.jpg',
+    'name' => 'Barnes',
+    'firstname' => 'Marguerite',
+    'age' => '44'
+  ],
+  [
+    'avatar' => 'images/avatar_2.jpg',
+    'name' => 'Baker',
+    'firstname' => 'Gabriel',
+    'age' => '33'
+  ],
+  [
+    'avatar' => 'images/avatar_3.jpg',
+    'name' => 'Barker',
+    'firstname' => 'Ola',
+    'age' => '34'
+  ],
+  [
+    'avatar' => 'images/avatar_4.jpg',
+    'name' => 'Lopez',
+    'firstname' => 'Rosie',
+    'age' => '49'
+  ],
+  [
+    'avatar' => 'images/avatar_1.jpg',
+    'name' => 'Toto',
+    'firstname' => 'Tata',
+    'age' => '15'
+  ],
+];
