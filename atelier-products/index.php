01d1ed1b09d485531ef9a9b5d80deef35b4160de
Lucas Delobelle <lucas@ld-web.net>
2022-01-06 16:18:27 +0100

    :construction: "page" GET param, redirect function

--- atelier-products/index.php
+++ atelier-products/index.php
@@ -1,4 +1,41 @@
-<?php require_once 'data/products.php'; ?>
+<?php
+require_once 'data/products.php';
+require_once 'functions.php';
+
+// Ici, on fixe le nombre de produits qu'on souhaite afficher par page.
+// On utilise une constante pour éviter toute modification ultérieure
+// de cette valeur.
+// Par ailleurs, il est utile de pouvoir nommer cette valeur (nom de la constante)
+// pour mieux comprendre, dans le code, à quoi correspond ce "5".
+const PRODUCTS_PER_PAGE = 5;
+
+// intval convertit une valeur donnée en valeur entière
+// Le paramètre GET "page" pouvant ne pas être présent dans l'URL,
+// alors il ne sera peut-être pas défini dans le tableau $_GET.
+// Ainsi, nous pouvons utiliser l'opérateur COALESCE (??),
+// pour indiquer à PHP que si $_GET['page'] n'existe pas, alors
+// on lui attribute la valeur 1.
+const DEFAULT_PAGE = 1;
+$page = intval($_GET['page'] ?? DEFAULT_PAGE);
+if ($page === 0) {
+  $page = DEFAULT_PAGE;
+}
+
+// Page 1 : départ = 0
+// Page 2 : départ = 5
+// Page 3 : départ = 10
+// ==> ($page - 1) * PRODUCTS_PER_PAGE
+$startIndex = ($page - 1) * PRODUCTS_PER_PAGE;
+
+// Je construis un sous-ensemble de produits en extrayant une portion de $products
+// Je pars de la clé de départ $startIndex calculée juste avant,
+// et souhaite intégrer à mon sous-ensemble uniquement le nombre de produits
+// que je veux afficher par page
+$displaySet = array_slice($products, $startIndex, PRODUCTS_PER_PAGE);
+if (empty($displaySet)) { // pas de résultat
+  redirect('index.php?page=1');
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -19,7 +56,7 @@
   </header>
   <main>
     <section class="list">
-      <?php foreach ($products as $product) {
+      <?php foreach ($displaySet as $product) {
         require 'product-item.php';
       } ?>
     </section>
