3985c5c585fe18a1d39f584ae51dfbb3777aedad
Lucas Delobelle <lucas@ld-web.net>
2022-01-06 14:35:50 +0100

    :sparkles: Atelier products, démarrage
    Liste de tous les produits

---
+++ atelier-products/product-item.php
@@ -0,0 +1,14 @@
+<article class="item">
+  <div class="picture">
+    <img src="assets/images/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" />
+  </div>
+  <div class="infos">
+    <h3><?php echo $product['name']; ?></h3>
+    <div class="categories">
+      <?php foreach ($product['categories'] as $category) { ?>
+        <p><?php echo $category; ?></p>
+      <?php } ?>
+    </div>
+    <h4><?php echo $product['priceHT']; ?> € TTC</h4>
+  </div>
+</article>
