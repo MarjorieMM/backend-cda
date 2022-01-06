f2191f9ec05c6bef3bd5012d68a8f3585b5eba66
Lucas Delobelle <lucas@ld-web.net>
2022-01-06 13:14:15 +0100

    :sparkles: GET param
    User display from ID param in URL

--- get_param.php
+++ get_param.php
@@ -2,28 +2,32 @@
 // Chargement des données
 require_once 'peoples.php';
 
+function searchForId(int $id, array $array): ?int
+{
+  foreach ($array as $key => $user) {
+    if ($user['id'] === $id) {
+      return $key;
+    }
+  }
+  return null;
+}
+
 // $_GET est un tableau associatif
 // Il faut donc accéder à ses indexes par les noms des paramètres d'URL
 // et non par des indexes numériques
 $id = $_GET['id'];
 
-var_dump($peoples[$id]);
+require_once 'layout/header.php';
 
-/*
-$peoples :
-[
-  [0] => [
-    'id' => 1,
-    'avatar' => ...,
-  ],
-  [1] => [
-    'id' => 45,
-    'avatar' => ...,
-  ],
-  [2] => [
-    'id' => 153,
-    'avatar' => ...,
-  ],
-  ...
-]
-*/
+// $userKey = searchForId($id, $peoples);
+
+$userKey = array_search($id, array_column($peoples, 'id'));
+
+if ($userKey !== false) {
+  $people = $peoples[$userKey];
+  require_once 'item.php';
+} else {
+  echo "Utilisateur non trouvé";
+}
+
+require_once 'layout/footer.php';
--- item.php
+++ item.php
@@ -1,6 +1,8 @@
 <article class="item">
   <div class="picture">
-    <img src="<?php echo $people['avatar']; ?>" alt="<?php echo $people['name'] . ' ' . $people['firstname']; ?>" />
+    <a href="get_param.php?id=<?php echo $people['id']; ?>">
+      <img src="<?php echo $people['avatar']; ?>" alt="<?php echo $people['name'] . ' ' . $people['firstname']; ?>" />
+    </a>
   </div>
   <div class="infos">
     <h3><?php echo $people['name'] . ' ' . $people['firstname']; ?></h3>
