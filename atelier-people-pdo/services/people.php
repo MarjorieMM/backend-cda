e393aabb08de3433da4d302b2112739a04190622
Lucas Delobelle <lucas@ld-web.net>
2022-01-14 13:13:13 +0100

    :bug: Fixed PHP8 syntax error in PHP7

--- atelier-people-pdo/services/people.php
+++ atelier-people-pdo/services/people.php
@@ -6,14 +6,22 @@ require_once 'db.php';
  *
  * @return PDOStatement|false
  */
-function findAllPeopleStatement(): PDOStatement|false
+function findAllPeopleStatement(): PDOStatement
 {
   $connection = getConnection();
   $query = "SELECT * FROM people";
   return $connection->query($query);
 }
 
-function addPeople(string $name, string $firstname, string $birthdate): PDOStatement|false
+/**
+ * Add a people to the database
+ *
+ * @param string $name
+ * @param string $firstname
+ * @param string $birthdate
+ * @return PDOStatement|false
+ */
+function addPeople(string $name, string $firstname, string $birthdate): PDOStatement
 {
   $connection = getConnection();
   $birthdateSqlVal = $birthdate === '' ? 'NULL' : "'" . $birthdate . "'";
